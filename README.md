e6a9f2a68c154c315855b0ef08e6e45739930267
Jin Azler <68141931+Zanzancomms@users.noreply.github.com>
2022-03-18 01:26:40 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -4,15 +4,9 @@ forked from Kataiser/RoR2-Rich-Presence [![Github all releases](https://img.shie
 Discord Rich Presence for Risk of Rain (thrown together from [Celeste Rich Presence](https://github.com/Kataiser/celeste-rich-presence), which itself is thrown together from an older version of [TF2 Rich Presence](https://github.com/Kataiser/tf2-rich-presence))
 
 ![Screenshot](screenshot.png)
-
-new features
--added new stages
--shows survivor in the small image
-
 ![Screenshot](image.png)
-
 (The actual program is nowhere near as nice looking as this)
 
 ## Download and running
-[RoR2_Rich_Presence_v2.0.zip](https://github.com/Zanzancomms/RoR2-Rich-Presence/releases/download/1.2/RoR2-Rich-Presence-2.0.zip) (8.6 MB)  
+[RoR2_Rich_Presence_v1.2.zip](https://github.com/Zanzancomms/RoR2-Rich-Presence/releases/download/1.2/RoR2-Rich-Presence-1.2.zip) (8.6 MB)  
 Run by running `run.bat` at the same time as the game.
--- main.py
+++ main.py
@@ -50,16 +50,82 @@ def main():
         if game_is_running and discord_is_running:
             if not client_connected:
                 # connects to Discord
-                client = ipc.DiscordIPC('566395208858861569')
+                client = ipc.DiscordIPC('954183647328620554')
                 client.connect()
                 client_connected = True
 
             with open(Player_log_path, 'r', errors='replace') as Player_log:
                 Player = Player_log.readlines()
 
-            old_details_state = copy.copy((activity['details'], activity['state']))
+            old_details_state = copy.copy((activity['details'], activity['state'], activity['assets']['small_image'], activity['assets']['small_text']))
 
             for line in Player:
+                    
+                if 'survivorIndex=8' in line:
+                    activity['assets']['small_image'] = 'commando'
+                    activity['assets']['small_text'] = 'Commando'
+
+                if 'survivorIndex=10' in line:
+                    activity['assets']['small_image'] = 'engineer'
+                    activity['assets']['small_text'] = 'Engineer'
+
+                if 'survivorIndex=12' in line:
+                    activity['assets']['small_image'] = 'huntress'
+                    activity['assets']['small_text'] = 'Huntress'
+
+                if 'survivorIndex=16' in line:
+                    activity['assets']['small_image'] = 'toolbot'
+                    activity['assets']['small_text'] = 'MUL-T'
+
+                if 'survivorIndex=14' in line:
+                    activity['assets']['small_image'] = 'mage'
+                    activity['assets']['small_text'] = 'Artificer'
+
+                if 'survivorIndex=15' in line:
+                    activity['assets']['small_image'] = 'merc'
+                    activity['assets']['small_text'] = 'Mercenary'
+
+                if 'survivorIndex=17' in line:
+                    activity['assets']['small_image'] = 'treebot'
+                    activity['assets']['small_text'] = 'REX'
+
+                if 'survivorIndex=6' in line:
+                    activity['assets']['small_image'] = 'bandit'
+                    activity['assets']['small_text'] = 'Bandit'
+
+                if 'survivorIndex=3' in line:
+                    activity['assets']['small_image'] = 'sniper'
+                    activity['assets']['small_text'] = 'Sniper'
+
+                if 'survivorIndex=2' in line:
+                    activity['assets']['small_image'] = 'han-d'
+                    activity['assets']['small_text'] = 'HAN-D'
+
+                if 'survivorIndex=3' in line:
+                    activity['assets']['small_image'] = 'han-d'
+                    activity['assets']['small_text'] = 'HAN-D'
+
+                if 'survivorIndex=9' in line:
+                    activity['assets']['small_image'] = 'acrid'
+                    activity['assets']['small_text'] = 'Acrid'
+
+                if 'survivorIndex=7' in line:
+                    activity['assets']['small_image'] = 'captain'
+                    activity['assets']['small_text'] = 'Captain'
+                    
+                if 'survivorIndex=18' in line:
+                    activity['assets']['small_image'] = 'railgunner'
+                    activity['assets']['small_text'] = 'Railgunner'
+
+                if 'survivorIndex=19' in line:
+                    activity['assets']['small_image'] = 'void_fiend'
+                    activity['assets']['small_text'] = '「V??oid Fiend』'
+
+                if 'survivorIndex=19' in line:
+                    activity['assets']['small_image'] = 'void_fiend'
+                    activity['assets']['small_text'] = '「V??oid Fiend』'
+                    
+
                 if 'Loaded scene' in line:
                     activity = switch_image_mode(activity)
 
@@ -73,21 +139,44 @@ def main():
                         activity['state'] = "Prismatic Trials"
 
                     elif 'golemplains' in line:
-                        activity = switch_image_mode(activity, ('golemplains', 'Titanic Plains'))
+                        activity = switch_image_mode(activity, ('golemplains', 'Golem Plains, Titanic Plains'))
                     elif 'blackbeach' in line:
-                        activity = switch_image_mode(activity, ('blackbeach', 'Distant Roost'))
+                        activity = switch_image_mode(activity, ('blackbeach', 'Black Beach, Distant Roost'))
+                    elif 'blackbeach2' in line:
+                        activity = switch_image_mode(activity, ('blackbeach2', 'Black Beach, Distant Roost'))
                     elif 'goolake' in line:
-                        activity = switch_image_mode(activity, ('goolake', 'Abandoned Aquaduct'))
+                        activity = switch_image_mode(activity, ('goolake', 'Goo Lake, Abandoned Aqueduct'))
                     elif 'frozenwall' in line:
-                        activity = switch_image_mode(activity, ('frozenwall', 'Rallypoint Delta'))
+                        activity = switch_image_mode(activity, ('frozenwall', 'Frozen Wall, Rallypoint Delta'))
                     elif 'dampcavesimple' in line:
-                        activity = switch_image_mode(activity, ('dampcavesimple', 'Abyssal Depths'))
+                        activity = switch_image_mode(activity, ('dampcavesimple', 'Tectonic Relics, Abyssal Depths'))
                     elif 'mysteryspace' in line:
                         activity = switch_image_mode(activity, ('mysteryspace', 'Hidden Realm: A Moment, Fractured'))
                     elif 'bazaar' in line:
                         activity = switch_image_mode(activity, ('bazaar', 'Hidden Realm: Bazaar Between Time'))
                     elif 'foggyswamp' in line:
-                        activity = switch_image_mode(activity, ('foggyswamp', 'Wetland Aspect'))
+                        activity = switch_image_mode(activity, ('foggyswamp', 'Foggy Swamp, Wetland Aspect'))
+                    elif 'wispgraveyard' in line:
+                        activity = switch_image_mode(activity, ('wispgraveyard', 'Wisp Graveyard, Scorched Acres'))
+                    elif 'shipgraveyard' in line:
+                        activity = switch_image_mode(activity, ('shipgraveyard', 'Ship Graveyard, Sirens Call'))
+                    elif 'skymeadow' in line:
+                        activity = switch_image_mode(activity, ('skymeadow', 'Sprite Fields, Sky Meadow'))
+                    elif 'snowyforest' in line:
+                        activity = switch_image_mode(activity, ('snowyforest', 'Siphoned Forest'))
+                    elif 'sulfurpools' in line:
+                        activity = switch_image_mode(activity, ('sulfurpools', 'Sulfur Pools'))
+                    elif 'rootjungle' in line:
+                        activity = switch_image_mode(activity, ('rootjungle', 'Root Jungle, Sundered Grove'))
+                    elif 'ancientloft' in line:
+                        activity = switch_image_mode(activity, ('ancientloft', 'Ancient Loft, Aphelian Sanctuary'))
+                    elif 'moon' in line:
+                        activity = switch_image_mode(activity, ('moon', 'Commencement , Moon of Petrichor V'))
+
+                    elif 'lobby' in line:
+                        activity = switch_image_mode(activity, ('lobby', 'Lobby'))
+                    elif 'title' in line:
+                        activity = switch_image_mode(activity, ('title', 'title'))
 
                 elif 'lobby creation succeeded' in line:
                     activity['details'] = "In lobby"
@@ -97,15 +186,86 @@ def main():
                     activity['details'] = "Main menu"
                     activity['state'] = "Not in lobby"
                     activity = switch_image_mode(activity)
+                    
+                if 'survivorIndex=8' in line:
+                    activity['assets']['small_image'] = 'commando'
+                    activity['assets']['small_text'] = 'Commando'
+
+                if 'survivorIndex=10' in line:
+                    activity['assets']['small_image'] = 'engineer'
+                    activity['assets']['small_text'] = 'Engineer'
+
+                if 'survivorIndex=12' in line:
+                    activity['assets']['small_image'] = 'huntress'
+                    activity['assets']['small_text'] = 'Huntress'
+
+                if 'survivorIndex=16' in line:
+                    activity['assets']['small_image'] = 'toolbot'
+                    activity['assets']['small_text'] = 'MUL-T'
+
+                if 'survivorIndex=14' in line:
+                    activity['assets']['small_image'] = 'mage'
+                    activity['assets']['small_text'] = 'Artificer'
+
+                if 'survivorIndex=15' in line:
+                    activity['assets']['small_image'] = 'merc'
+                    activity['assets']['small_text'] = 'Mercenary'
+
+                if 'survivorIndex=17' in line:
+                    activity['assets']['small_image'] = 'treebot'
+                    activity['assets']['small_text'] = 'REX'
+
+                if 'survivorIndex=6' in line:
+                    activity['assets']['small_image'] = 'bandit'
+                    activity['assets']['small_text'] = 'Bandit'
+
+                if 'survivorIndex=3' in line:
+                    activity['assets']['small_image'] = 'sniper'
+                    activity['assets']['small_text'] = 'Sniper'
+
+                if 'survivorIndex=2' in line:
+                    activity['assets']['small_image'] = 'han-d'
+                    activity['assets']['small_text'] = 'HAN-D'
+
+                if 'survivorIndex=3' in line:
+                    activity['assets']['small_image'] = 'han-d'
+                    activity['assets']['small_text'] = 'HAN-D'
+
+                if 'survivorIndex=9' in line:
+                    activity['assets']['small_image'] = 'acrid'
+                    activity['assets']['small_text'] = 'Acrid'
+
+                if 'survivorIndex=7' in line:
+                    activity['assets']['small_image'] = 'captain'
+                    activity['assets']['small_text'] = 'Captain'
+                    
+                if 'survivorIndex=18' in line:
+                    activity['assets']['small_image'] = 'railgunner'
+                    activity['assets']['small_text'] = 'Railgunner'
+
+                if 'survivorIndex=19' in line:
+                    activity['assets']['small_image'] = 'void_fiend'
+                    activity['assets']['small_text'] = '「V??oid Fiend』'
+
+                if 'survivorIndex=19' in line:
+                    activity['assets']['small_image'] = 'void_fiend'
+                    activity['assets']['small_text'] = '「V??oid Fiend』'
+                    
+
+
+
 
             if time.time() - start_time < 10:
                 activity['details'] = "Loading game"
 
-            if old_details_state != (activity['details'], activity['state']):
+            if old_details_state != (activity['details'], activity['state'], activity['assets']['small_image'], activity['assets']['small_text']):
                 next_delay = 2
 
             print(activity['details'])
             print(activity['state'])
+            print(activity['assets']['small_image'])
+            print(activity['assets']['small_text'])
+            
             time_elapsed = time.time() - start_time
             print("{:02}:{:02} elapsed\n".format(int(time_elapsed / 60), round(time_elapsed % 60)))
 
@@ -144,11 +304,9 @@ def main():
 
 def switch_image_mode(temp_activity, stage=()):
     if stage == ():
-        temp_activity['assets']['small_image'] = ' '
         temp_activity['assets']['large_image'] = 'logo'
         temp_activity['assets']['large_text'] = 'Risk of Rain 2'
     else:
-        temp_activity['assets']['small_image'] = 'icon'
         temp_activity['assets']['large_image'] = stage[0]
         temp_activity['assets']['large_text'] = stage[1]
         temp_activity['details'] = stage[1]
